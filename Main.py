8a5f7be1549c7d87cf837b6431676a066d75dc07
GavinMcyD <Gav@trotzke.com>
2026-03-07 17:48:18 -0500

    basic data

---
+++ Main.py
@@ -0,0 +1,14 @@
+import csv
+
+employees = [
+["Sarah Colin",14569,0,40,20],
+["Anna Johnson",14572,0,40,22],
+["Jessica Dean",14580,3,40,18],
+["Taylor Cane",14528,1,40,16],
+["Jackson Davis",14530,0,60,16],
+["Austin Joan",14532,2,40,16],
+["Nathan Lee",14582,0,40,16],
+["Bailey Walt",14563,0,50,22],
+["Brittney Greene",14520,4,40,18],
+["Leann Clancy",14519,0,50,22]
+]
